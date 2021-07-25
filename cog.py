5f687b698fe1b2e1b5fc16801ed9bb403b87a891
atinching <atin5551@gmail.com>
2021-07-25 21:50:57 +0300

    Added message xp and many commands
    - Made it so that every message gives the author of the message a certain amount of exp. By default it is 20, can be changed with set_xp_per_message()
    - Implemented command set_xp_per_message, lets you change the amount of xp given to someone for one message.
    - Implemented command set_bonus_xp_days, sets the days that bonus xp is given out
    - Implemented command unset_bonus_xp_days, unsets the days bonus xp is given out.
    - Implemented command status, that returns an embed containing information about the bots status (whether its connected to the database or not, the bots latency, etc.)

--- cog.py
+++ cog.py
@@ -1,3 +1,4 @@
+from asyncio import windows_events
 import discord
 from discord import user
 from discord.ext import commands
@@ -7,6 +8,8 @@ import datetime
 import pymongo
 from pymongo import MongoClient
 
+
+
 class Levelcog(commands.Cog):   
 
     def __init__(self, bot):
@@ -15,6 +18,9 @@ class Levelcog(commands.Cog):
         self.client = None
         self.collection = None
         self.connected = False # Whether the bot is connected to the db.
+        self.xp_per_message = 20
+        self.bonus_xp_days = []
+        self.valid_days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
         self.connect_to_db()
 
     def connect_to_db(self):
@@ -63,17 +69,36 @@ class Levelcog(commands.Cog):
                     'name' : user.name,
                     '_id' : user.id, # _id (which is the primary key/identifier/unique field among all member entries in the database) is the discord user's id.
                     'level' : 1,
-                    'xp' : 0,
+                    'normal_xp' : 0,
+                    'bonus_xp' : 0,
+                    'total_xp' : 0, 
                     'background' : None 
         })
-        return True # Could register user into the db        
-            
-    
+        return True # Could register user into the db    
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        author = message.author
+        if author.bot: return
+        if self.connected: 
+            entry = self.collection.find_one({'_id' : author.id}) 
+            if entry != None:
+                self.collection.update_one({'_id' : author.id}, {'$inc' : {'xp' : self.xp_per_message}, '$set' : {'level' : self.determine_level(entry['xp'] + self.xp_per_message)}})
+
+    @commands.command()
+    async def status(self, ctx, *args): # Returns embed containing the bot's status, like its connection to the database, latency etc.
+        embed = discord.Embed(title='Bot Status')
+        embed.set_footer(text=str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+        embed.add_field(name='Connection to database', value=self.connected, inline=True)
+        embed.add_field(name='Latency', value=self.bot.latency, inline=True)
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+        await ctx.send(embed=embed)
+
     @commands.command()
     async def initialise(self, ctx, *args):
         embed : discord.Embed = discord.Embed(title="Members added to the database", description='') # Embed that stores all the members added to the database.
         for m in self.bot.guilds[0].members: # Bot is only in 1 server/guild thats why we can use self.bot.guilds[0]
-            if self.collection.find_one({'_id' : m.id}) == None and not m.bot: # If the member could not be found in the db, then find_one() returns NoneType, so that's how we know the member doesn't exist in the db.
+            if self.collection.find_one({'_id' : m.id}) == None: # If the member could not be found in the db, then find_one() returns NoneType, so that's how we know the member doesn't exist in the db.
                 if self.connected: # Registering user into db. If they can't then that means a connection to the db couldn't be established
                     self.register_user(m)
                     embed.description += "\n " + m.mention
@@ -136,7 +161,6 @@ class Levelcog(commands.Cog):
         if self.connected:
             leaderboard = list(self.collection.find({}).sort('xp', pymongo.DESCENDING).limit(10))
             total_count = len(leaderboard)
-            embed = discord.Embed()
             embed_list = []
             i = 0
             while total_count >= 1: # Keep making pages as long as there are user entries left.
@@ -157,7 +181,40 @@ class Levelcog(commands.Cog):
                     #await msg_send.add_reaction(":arrow_left:")
                     #await msg_send.add_reaction(":x:")
 
+    @commands.command()
+    async def set_bonus_xp_days(self, ctx, args):
+        for arg in args:
+            if self.connected and arg.lower() in self.valid_days: 
+                arg_lower = arg.lower()
+                if arg_lower not in self.bonus_xp_days: # If the current day entered IS NOT in the already active bonus days
+                    self.bonus_xp_days.append(arg_lower) # We add it
+                    await ctx.send(arg + " has been set as a bonus xp day!")
+                else: await ctx.send(arg + ' is already a bonus xp day!') # Otherwise it's already one of the active bonus xp days. 
+            elif not self.connected: await ctx.send("The bot could not connect to the database.")
+            else: await ctx.send('You sent in an invalid day!')
+    
+    @commands.command()
+    async def unset_bonus_xp_days(self, ctx, args):
+        for arg in args:
+            if self.connected and arg.lower() in self.valid_keys:
+                arg_lower = arg.lower()
+                if arg_lower in self.bonus_xp_days: # If the current day entered IS in the already active bonus days
+                    self.bonus_xp_days.remove(arg_lower) # We remove it.
+                    await ctx.send(arg + ' is no longer a bonus xp day!')
+                else: await ctx.send(arg + ' is not a bonus xp day!')
+            elif not self.connected: await ctx.send("The bot could not connect to the database.")
+            else: await ctx.send('You sent in an invalid day!')
 
+    @commands.command()
+    async def set_xp_per_message(self, ctx, xp, args):
+        try:
+            xp = int(xp) # Trying to convert the xp arg to a number
+            if xp < 0: raise ValueError # We can't have negative xp for a number
+        except(ValueError): # ValueError is thrown if xp couldn't be converted to int.
+            await ctx.send('You sent in an invalid number!')
+            return
+        self.xp_per_message = xp
+        await ctx.send('The amount of xp given per message has been set to ' + str(xp))
 
 def setup(client):
     client.add_cog(Levelcog(client))
